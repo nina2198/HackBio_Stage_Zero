aaa00228e9cd0f4f44d897730714e64aa7a102ff
Nina <57954425+nina2198@users.noreply.github.com>
2026-04-24 23:11:51 +0200

    Add files via upload

--- stage_zero.py
+++ stage_zero.py
@@ -1,1 +1,11 @@
+name = "Banguket Nina Tchoucnou"
+affiliation =  "Hasselt University"
+gene = "omega-1"
+organism = "Schistosoma mansoni"
+common_name = "Blood fluke"
 
+print(
+    f"My name is {name} and I am a masters student at {affiliation}.\n"
+    f"The gene I find the most interesting is {gene} in \033[3m{organism}\033[0m," 
+    f"commonly known as {common_name}."
+)
